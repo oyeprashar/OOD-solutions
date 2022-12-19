cd10871de71bdfb84d92729030a0c2394df0b164
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2022-12-19 12:46:03 +0530

    Update TinyUrl.py

--- TinyUrl.py
+++ TinyUrl.py
@@ -1,101 +1,75 @@
 """
 1. Requirement gathering and use cases
-	
+
 	- The user can do two things
 		a. Give the long url and ask for the tiny url
 		b. Give the short url and expect to receive the longer one
-
-
-
 	Approach
 		1. if we have already generated a short url for the input long url then we return that
 		2. id = len(shortUrlToLong)
 		3. use the id to generate the shortURL and map the longURL to shortURL
 		4. Use shortURL and return the longURL
-
-
 2. Identifying the entities and services
-			
 
 	Services
 		a. LinkShortner
 			+ getShortURL(self,websiteObject)	
 			+ getLongURL(self,websiteObject)
-
 3. Designing
 4. Refinement
 """
 
 from abc import ABC, abstractmethod
 
-class AbstractLinkShortner(ABC):
+
+class URLshortner(ABC):
 
 	@abstractmethod
-	def getShortURL(self,longUrl):
+	def getShortURL(self, longURL):
 		pass
 
 	@abstractmethod
-	def getLongUrl(self,shortUrl):
+	def getLongURL(self, shortURL):
 		pass
 
 
-class LinkShortner(AbstractLinkShortner):
-
-	__instance = None
-	longToShortUrl = {}
-	shortToLongUrl = {}
-	baseUrl = "tinyUrl.com/"
-
-	def getInstance():
-
-		if LinkShortner.__instance == None:
-			LinkShortner.__instance = LinkShortner()
-
-		return LinkShortner.__instance
+class BitLy(URLshortner):
 
 	def __init__(self):
+		self.shortToLongUrl = {}
 
-		if LinkShortner.__instance != None:
-			raise Exception("Object already exists! Please use getInstance() to get the object!")
-
-		LinkShortner.__instance = self
-
-	def getShortURL(self,longUrl):
-
-		if longUrl in LinkShortner.longToShortUrl:
-			return self.baseUrl + LinkShortner.longToShortUrl[longUrl]
-
-		chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-		uniqueId = len(LinkShortner.longToShortUrl) + 1 # Cannot be zero since the below function stops when this id becomes 0
-		shortUrl = ""
+	# we can generate 62^7 = 3.5 Trillion
+	def getEncodedString(self, uniqueId):
+		char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		ans = ""
 
 		while uniqueId > 0:
-			shortUrl += chars[uniqueId%62]
+			ans += char[uniqueId % 62]
 			uniqueId //= 62
 
-		LinkShortner.longToShortUrl[longUrl] = shortUrl
-		LinkShortner.shortToLongUrl[shortUrl] = longUrl
+		return ans
 
-		return LinkShortner.baseUrl + shortUrl
+	def getShortURL(self, longURL):
 
-	def getLongUrl(self,shortUrl):
+		uniqueId = len(self.shortToLongUrl) + 1
+		shortURL = self.getEncodedString(uniqueId)
 
-		if len(shortUrl) == 0:
-			return "Invalid URL"
+		self.shortToLongUrl[shortURL] = longURL
 
-		domainParts = shortUrl.split('/')
+		return "bit.yl/" + shortURL
 
-		if len(domainParts) == 1:
-			return "Invalid URL"
+	def getLongURL(self, shortURL):
 
-		shortCode = domainParts[1]
+		uniqueString = shortURL.split('/')[1]
 
-		if shortCode not in LinkShortner.shortToLongUrl:
-			return "404"
+		if uniqueString not in self.shortToLongUrl:
+			return shortURL
 
-		return LinkShortner.shortToLongUrl[shortCode]
+		return self.shortToLongUrl[uniqueString]
 
 
-shortner = LinkShortner()
-print(shortner.getShortURL("google.com"))
-print(shortner.getLongUrl("tinyUrl.com/b"))
+shortner = BitLy()
+longUrl = "facebook.com"
+shortUrl = shortner.getShortURL(longUrl)
+print("shortURL = ", shortUrl)
+print("longUrl =", shortner.getLongURL(shortUrl))
